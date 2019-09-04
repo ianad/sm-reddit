79c9d4bd3283bb82b36062d6e76e8cebf0fa4fbd
ianad <ian.dalton@ucf.edu>
2019-09-04 16:45:42 -0400

    add gcloud secrets

--- notebooks/secrets.py
+++ notebooks/secrets.py
@@ -1,3 +1,16 @@
 user_agent=""
 client_id=""
 client_secret=""
+
+google_cloud_secrets = {
+  "type": "",
+  "project_id": "",
+  "private_key_id": "",
+  "private_key": "",
+  "client_email": "",
+  "client_id": "",
+  "auth_uri": "",
+  "token_uri": "",
+  "auth_provider_x509_cert_url": "",
+  "client_x509_cert_url": ""
+}
