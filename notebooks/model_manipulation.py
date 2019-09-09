bf4c91bfc68ef7c0383be81ab9ed041d7d5e0370
ianad <ian.dalton@ucf.edu>
2019-09-09 17:10:48 -0400

    add naive jargon score framework

--- notebooks/model_manipulation.py
+++ notebooks/model_manipulation.py
@@ -2,6 +2,9 @@ import praw
 from _secrets import user_agent, client_id, client_secret
 import pandas as pd
 from tqdm import tqdm
+import numpy as np
+
+
 
 def initialize_reddit(agent_string = user_agent):
     reddit = praw.Reddit(user_agent = agent_string,
@@ -36,3 +39,46 @@ def big_comments_pull():
         df.append(pd.DataFrame([c.__dict__ for c in comments_tree]))
 
     df.to_csv('comments.csv')
+
+    
+def jargon_score(obj):
+    # Try to cast 'obj' as numpy array
+    try: array_obj = np.array(obj).astype('str')
+    except: TypeError(f"`obj` should be castable as NumPy array-like of strings, not {type(obj)}")
+    
+    # Another test: `obj` should also be 1-dimensional
+    if array_obj.ndim != 1:
+        raise ValueError("`obj` should be 1-dimensional")
+    
+    # placeholder dictionary
+    english_dictionary = ['a', 'the', 'but', 'yes', 'no', 'fizz', 'buzz', 'hello', 'world']
+        
+    # defining Rules as lambda functions in a list so len(ruleset) can be referenced later
+    # each returning a boolean value cast as a float to allow for later weighting
+    ruleset = [
+        lambda token: float(str.isupper(token[0])),
+        lambda token: float(token not in english_dictionary)
+    ]
+
+    # Create dummy scores object 
+    # Should be 2 dimensional 
+    ## dim_1 = len(array_obj) 
+    ## dim_2 = len(ruleset)
+    scores_obj = np.empty(
+        (array_obj.shape[0],
+         len(ruleset))
+    )
+    
+    
+
+    for t, token in enumerate(array_obj):
+        for r, rule in enumerate(ruleset):
+            score = rule(token)
+            # apply each rule lambda in `ruleset` to each token in `array_obj`
+            scores_obj[t,r] = score
+            pass
+    
+    # Return final scores obj
+    # Alternatively, mean out all the scores and return a 1-dimensional array same shape with `obj`
+    return(scores_obj)
+
